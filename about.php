4ad13960568c6d493c2fb90c7888cfcdf402f306
= <arthurtwmt.com.au>
2021-01-10 22:51:35 +1000

    Create about page

---
+++ about.php
@@ -0,0 +1,22 @@
+<?php include("dbconnect.php"); ?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>The Advice Shop - About</title>
+<link href="styles/mainstyles.css" rel="stylesheet" type="text/css" media="screen">
+</head>
+
+<body>
+<?php include("inc_header.php");
+include("inc_nav.php"); ?>
+<h2>About</h2>
+<div id="content">
+  <p>The advice that you need and the right time can do wonder!</p>
+    <p> Use the advice shop to change your life with the best possible advice</p>
+</div>
+
+<p>&nbsp;</p>
+<?php include("inc_footer.php"); ?>
+</body>
+</html>
